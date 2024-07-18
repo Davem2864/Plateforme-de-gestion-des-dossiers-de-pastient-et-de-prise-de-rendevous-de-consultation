13da3a0374f54662245fd5e4b6537e2d54e144e6
David Mbuluku <mbuluku2864@gmail.com>
2024-07-19 01:11:35 +0200

    la connection au server se fais sans embuche si vous n'avez encore specifié le mot de passe de connexion

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","Mbuluku@0022","edoc");
+    $database= new mysqli("localhost","root","","edoc");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
